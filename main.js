66e50f37127674b25ed49906b8c2352a5b61d6bc
Abduvohidov Bilolliddin <bilol200414@gmail.com>
2021-11-11 16:12:53 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var myName1 = "bilol";
+
+var myName = prompt("Ismizi kiriting!");
+
+if(myName == myName1){
+    console.log("Assalamu Aleikum");
+} else{
+    console.log("Assalamu Aleikum aka");
+}
